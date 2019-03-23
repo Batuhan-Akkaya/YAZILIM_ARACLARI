6a8dc5fa01890d5f6cebcd1c79a868ffcefa5d6c
bprlab <bprlab@192.168.5.239>
2019-03-23 13:27:41 +0300

    Hesap test duzenleme

--- OrnekMavenProje/src/test/java/HesapTest.java
+++ OrnekMavenProje/src/test/java/HesapTest.java
@@ -5,24 +5,16 @@ public class HesapTest {
 	// @org.junit.Test
 	@Test
 	public void testTopla() {
-		
 		Hesap h = new Hesap();
-		
 		int sonuc = h.topla(345, 453);
-		
 		Assert.assertEquals(798, sonuc);
-		
 	}
 
 	@Test
 	public void testFaktoriyel() {
-		
 		Hesap h = new Hesap();
-		
 		int sonuc = h.faktoriyel(10);
-		
-		Assert.assertEquals(3628801, sonuc);
-		
+		Assert.assertEquals(3628800, sonuc);
 	}
 	
 }
